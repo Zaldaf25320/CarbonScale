a3906e6c19314fdd31a1cd78d22a095bb74a7e3d
theov <theo.vuillemin@viacesi.fr>
2023-11-18 18:38:20 +0100

    endpoint ressource 3

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -36,8 +36,7 @@ class CategorieController extends AbstractController
     #[Route('/api/{categorie}/ressource', name: 'app_api_categorie_ressource', methods: 'GET')]
     public function getRessourcesByCategorie($categorie): Response
     {
-        $ressources = $this->ressourceRepository->find($this->categorieRepository->findOneBy(['intitule' => $categorie]));
-
+        $ressources = $this->ressourceRepository->findOneBy($this->categorieRepository->findBy(['intitule' => $categorie]));
         $themeJson = $this->Serializer->serialize($ressources,"json", ["groups" => "res"]);
         $response = New Response();
         $response->setStatusCode(Response::HTTP_OK);
