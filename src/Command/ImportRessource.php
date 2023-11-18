cbd78743e1bbcf5b03f1f3fb64c15243d2f715f5
Paul <Paul.etignard@gmail.com>
2023-11-18 16:49:06 +0100

    Ajout des catégories et des

--- src/Command/ImportRessource.php
+++ src/Command/ImportRessource.php
@@ -3,7 +3,9 @@
 
 namespace App\Command;
 
+use App\Entity\Categorie;
 use App\Entity\Ressource;
+use App\Repository\CategorieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use League\Csv\Statement;
 use Symfony\Component\Console\Attribute\AsCommand;
@@ -24,31 +26,41 @@ use League\Csv\Reader;
 class ImportRessource extends Command
 {
     private EntityManagerInterface $manager;
+    private CategorieRepository $categorieRepository;
 
-    public function __construct(EntityManagerInterface $manager)
+    public function __construct(EntityManagerInterface $manager, CategorieRepository $categorieRepository)
     {
         $this->manager = $manager;
+        $this->categorieRepository = $categorieRepository;
         parent::__construct();
     }
 
     protected function execute(InputInterface $input,OutputInterface $output): int
     {
 
-        $output->writeln([
-            'Importation ressource'
-        ]);
 
         $csv = Reader::createFromPath('documentation/basecarbonescale.csv', 'r');
         $csv->setDelimiter(';');
         $csv->setHeaderOffset(0);
 
 
+
+
+        $output->writeln([
+            'Importation ressource'
+        ]);
+
+
+
         foreach ($csv as $import ) {
 
                 $ressource = new Ressource();
                 $ressource->setIntitule($import['nom']);
-                $ressource->setPoid($import['conso en Kg Co2e']);
+                $poid = floatval($import['conso en Kg CO2e']);
+                $ressource->setPoid($poid);
                 $ressource->setUnite($import['type de calcule']);
+
+                $ressource->setCategorie($this->categorieRepository->findOneBy(['intitule' => $import['Type']]));
                 $this->manager->persist($ressource);
 
         }
