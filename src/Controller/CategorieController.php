8178309d7b7a3763de3c084f1af053d5b012caf2
theov <theo.vuillemin@viacesi.fr>
2023-11-19 10:57:23 +0100

    endpoint action

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Repository\ActionRepository;
 use App\Repository\CategorieRepository;
 use App\Repository\RessourceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -13,12 +15,14 @@ class CategorieController extends AbstractController
 {
     private CategorieRepository $categorieRepository;
     private RessourceRepository $ressourceRepository;
+    private ActionRepository $actionRepository;
 
-    public function __construct(CategorieRepository $categorieRepository, SerializerInterface $Serializer,RessourceRepository $ressourceRepository)
+    public function __construct(CategorieRepository $categorieRepository, SerializerInterface $Serializer,RessourceRepository $ressourceRepository,ActionRepository $actionRepository)
     {
         $this->categorieRepository = $categorieRepository;
         $this->Serializer = $Serializer;
         $this->ressourceRepository = $ressourceRepository;
+        $this->actionRepository = $actionRepository;
     }
     #[Route('/api/categories', name: 'app_api_categories', methods: 'GET')]
     public function getAllCategories(): Response
@@ -45,4 +49,26 @@ class CategorieController extends AbstractController
         $response->setContent($themeJson);
         return $response;
     }
+
+    #[Route('/api/action', name: 'app_api_post_action', methods: ['POST'])]
+    public function postAction(Request $request): Response
+    {
+        $returnActions = [];
+        $data = $request->toArray();
+        foreach ($data as $key => $actionId) {
+
+            $returnActions[] = [
+                "key" => $key,
+                "id" => $actionId,
+                "nombreRessources" => $this->actionRepository->find($actionId)->getNombreRessources(),
+                "ressource" => $this->actionRepository->find($actionId)->getRessource()->getId(),
+                "user" => $this->actionRepository->find($actionId)->getUser()->getId()
+            ];
+        }
+
+        return new Response(json_encode($returnActions) , 200, []);
+    }
+
+
+
 }
